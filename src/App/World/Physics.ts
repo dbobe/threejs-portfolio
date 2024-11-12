412db714e08b940be5cbe1141fa239337ded19e3
dbobe <bobe.david@live.com>
2024-11-11 19:14:45 -0500

    Physics Dynamic Object

--- src/App/World/Physics.ts
+++ src/App/World/Physics.ts
@@ -1,21 +1,53 @@
 import * as THREE from "three";
 import App from "../App";
+import type { World, RigidBody } from "@dimforge/rapier3d";
 
 export default class Physics {
   app: App;
   scene: THREE.Scene | undefined;
-  world: any;
+  world: World | undefined;
+  rigidBody: RigidBody | undefined;
+  rapierLoaded: boolean = false;
+  cubeMesh:
+    | THREE.Mesh<
+        THREE.BoxGeometry,
+        THREE.MeshStandardMaterial,
+        THREE.Object3DEventMap
+      >
+    | undefined;
 
   constructor() {
     this.app = new App();
     this.scene = this.app.scene;
 
     import("@dimforge/rapier3d").then((RAPIER) => {
-      const gravity = { x: 0, y: -9.81, z: 0 };
+      const gravity = new RAPIER.Vector3(0, -9.81, 0);
       this.world = new RAPIER.World(gravity);
-      console.log(this.world);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshStandardMaterial({ color: "blue" });
+      this.cubeMesh = new THREE.Mesh(geometry, material);
+      this.scene?.add(this.cubeMesh);
+
+      // rigid body
+      const rigidBodyType = RAPIER.RigidBodyDesc.dynamic();
+      this.rigidBody = this.world.createRigidBody(rigidBodyType);
+
+      // collider
+      const colliderType = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
+      this.world.createCollider(colliderType, this.rigidBody);
+
+      this.rapierLoaded = true;
     });
   }
 
-  loop() {}
+  loop() {
+    if (!this.rapierLoaded) return;
+    this.world?.step();
+
+    const position = this.rigidBody?.translation();
+    const rotation = this.rigidBody?.rotation();
+    this.cubeMesh?.position.copy(position!);
+    this.cubeMesh?.quaternion.copy(rotation!);
+  }
 }
